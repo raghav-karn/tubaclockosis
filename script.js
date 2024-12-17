c2de4794b34282c2271163aa91022616fc36230f
raghav-karn <raghavkarn@outlook.com>
2024-12-17 13:13:36 +0530

    Scripted base

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateTimeAndDate() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    let amPm = hours >= 12 ? 'PM' : 'AM';
+}  
