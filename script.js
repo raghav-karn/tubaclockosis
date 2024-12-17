3d8cea24dbcccedbd529f6a09b9ab2634537c124
raghav-karn <raghavkarn@outlook.com>
2024-12-17 13:14:17 +0530

    Scripted 12hr

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function updateTimeAndDate() {
     let hours = now.getHours();
     const minutes = now.getMinutes().toString().padStart(2, '0');
     let amPm = hours >= 12 ? 'PM' : 'AM';
+    if (hours > 12) {
+        hours -= 12;
+    } else if (hours === 0) {
+        hours = 12;
+    }
+  
 }  
